fe1d998084907c76ffb385dc0812e78ea8d07ccb
Sina Haji Kazemi <sina.hkazemi@gmail.com>
2022-04-25 12:03:14 +0430

    optimization added

--- opticut/optimization.py
+++ opticut/optimization.py
@@ -1,14 +1,34 @@
+from typing import Dict, List, Union, NamedTuple
 import pyomo.environ as pyo
+
+class Pattern(NamedTuple):
+    length : int
+    pieces: Dict[int,int]
+
+class Algorithm():
+    def __init__(self, solver: pyo.SolverFactory,
+            pieces: Dict[int,int],
+            bars: Dict[int, Union[None,int]] = None,
+            std_bar: int = None,
+            blade_width: int = 0,
+            pattern_num: int = None,
+            blade_num: int = None) -> None:
+        pass
+    def solve(self):
+        return 
 
 
 class Parent():
-    def __init__(self, solver: pyo.SolverFactory) -> None:
+    def __init__(self, solver: pyo.SolverFactory, pieces: Dict[int,int], initial_paterns: List[Pattern]) -> None:
         self.solver = solver
     
 
         
 
 class Child():
-    def __init__(self, solver: pyo.SolverFactory) -> None:
+    def __init__(self, solver: pyo.SolverFactory, lengths: List[int], blade_width: int, blade_num: Union[int,None]) -> None:
         self.solver = solver
+        self.blade_width = blade_width
+        self.blade_num = blade_num
+
     
--- opticut/script.py
+++ opticut/script.py
@@ -8,7 +8,7 @@ class SolverError(ValueError):
     
 
 class CuttingStock():
-    def __init__(self, solver_name:str='glpk') -> None:
+    def __init__(self, solver_name:str) -> None:
         if not pyo.SolverFactory(solver_name).available():
             raise SolverError(solver_name)
         else:
@@ -18,10 +18,10 @@ class CuttingStock():
     def solve(
             pieces: Dict[int,int],
             bars: Dict[int, Union[None,int]] = None,
-            std_bar: int = None,
+            std_bar: Union[None,int] = None,
             blade_width: int = 0,
-            pattern_num: int = None,
-            blade_num: int = None
+            pattern_num: Union[None,int] = None,
+            blade_num: Union[None,int] = None
         ) -> Dict: 
         """_summary_
 
@@ -29,13 +29,13 @@ class CuttingStock():
         :type pieces: Dict[int,int]
         :param bars: dict of the available bars' length and their corresponding number, If the specified number is None there is no limit. defaults to None
         :type bars: Dict[int, Union[None,int]], optional
-        :param std_bar: _description_, defaults to None
+        :param std_bar: tandard length of the bar. Just one of the bars and std_bar should be given, defaults to None
         :type std_bar: int, optional
-        :param blade_width: _description_, defaults to 0
+        :param blade_width:  the width of the cutting blade, defaults to 0
         :type blade_width: int, optional
-        :param pattern_num: _description_, defaults to None
+        :param pattern_num: maximum number of the used patterns and if is None there is no limit, defaults to None
         :type pattern_num: int, optional
-        :param blade_num: _description_, defaults to None
+        :param blade_num:  maximum number of the cutting blades and if is None there is no limit, defaults to None
         :type blade_num: int, optional
         :return: _description_
         :rtype: Dict
@@ -44,18 +44,3 @@ class CuttingStock():
 
         pass
 
-
-    # """solves 
-
-    # Args:
-    #     :param pieces (Dict[int,int]): dict of pieces and their corresponding numbers.
-    #     :param bars (Dict[int, Union[None,int]], optional): dict of the available bars' length and their corresponding number. If the specified number is None there is no limit. Defaults to None.
-    #     :param std_bar (int, optional): standard length of the bar. Just one of the bars and std_bar should be given. Defaults to None.
-    #     :param blade_width (int, optional): the width of the cutting blade. Defaults to 0.
-    #     :param pattern_num (int, optional): maximum number of the used patterns and if is None there is no limit. Defaults to None.
-    #     :param blade_num (int, optional): maximum number of the cutting blades and if is None there is no limit. Defaults to None.
-
-    # Returns:
-    #     Dict: _description_
-
-    # """
