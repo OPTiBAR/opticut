8f38b212079beb80b0d2be582eefb0c500a2a41b
Sina Haji Kazemi <sina.hkazemi@gmail.com>
2022-04-27 20:03:18 +0430

    optimization algorithm changed

--- opticut/optimization.py
+++ opticut/optimization.py
@@ -1,45 +1,61 @@
-from ast import pattern
-from typing import Dict, List, Union, NamedTuple
+from typing import Dict, List, Tuple, NamedTuple
 import pyomo.environ as pyo
 import math
 
-class Pattern(NamedTuple):
-    length : int
-    pieces: Dict[int,int]
 
-class Algorithm():
+class Pattern():
+    def __init__(self, length : int, pieces: Dict[int,int] ) -> None:
+        self.length = length
+        self.pieces = pieces
+
+class CG():
+    """Column Generation algorithm for cutting stock problem
+    """
     def __init__(self, solver: pyo.SolverFactory,
             pieces: Dict[int,int],
-            bars: Dict[int, Union[None,int]],
+            bars: Dict[int,int],
             blade_width: int = 0,
-            pattern_num: int = None,
-            blade_num: int = None) -> None:
+            pattern_num: int = -1,
+            blade_num: int = -1
+            ) -> None:
+        self.solver = solver
         self.pieces = pieces
         self.bars = bars
         self.blade_width = blade_width
         self.pattern_num = pattern_num
         self.blade_num = blade_num
         
-    def solve(self):
-        return
-    
-    def get_initial_patterns(self):
+    def solve(self) -> List[Tuple[Pattern, int]]:
+        initial_patterns = self._get_initial_patterns()
+        parent = Parent(self.solver, pieces=self.pieces, initial_patterns=initial_patterns, pattern_num=self.pattern_num, bars= self.bars)
+        child = Child(solver=self.solver, lengths=list(self.pieces.keys()), blade_width=self.blade_width, blade_num=self.blade_num)
+        
+        while True:
+            parent.solve(domain=pyo.NonNegativeReals)
+            for bar_length in self.bars.keys():
+                result = child.solve(bar_length=bar_length, duals=parent.get_duals())
+                if bar_length - result['obj'] > 1e-4:
+                    parent.add_pattern(result['pattern'])
+                    break # for loop
+            else:
+                break # while loop
+        parent.solve(domain=pyo.NonNegativeIntegers)
+        return parent.get_values()
+
+    def _get_initial_patterns(self) -> List[Pattern]:
         patterns = []
-        bar_len = max(filter(lambda length: self.bars[length] is None, self.bars)) # find longest unlimited bar
+        print(self.bars)
+        bar_len = max(filter(lambda length: self.bars[length] == -1, self.bars)) # find longest unlimited bar
         for piece_len in self.pieces.keys():
             patterns.append(Pattern(bar_len, {piece_len:math.floor(bar_len/piece_len)}))
         return patterns
 
-
-
-
-
 class Parent():
-    def __init__(self, solver: pyo.SolverFactory, pieces: Dict[int,int], initial_patterns: List[Pattern], pattern_num: Union[int,None], bars:Dict[int, Union[None,int]]) -> None:
+    def __init__(self, solver: pyo.SolverFactory, pieces: Dict[int,int], initial_patterns: List[Pattern], pattern_num: int, bars:Dict[int, int]) -> None:
         self.solver = solver
         self.patterns = initial_patterns
         self.pieces = pieces
-        self.results = {}
+        self.results = {'values': {}, 'obj': None, 'duals': None}
         self.pattern_num = pattern_num
         self.bars = bars
 
@@ -47,20 +63,26 @@ class Parent():
         model = pyo.ConcreteModel()
         model.x = pyo.Var(self.patterns, within=domain)
 
+        # initialize the x variables
+        for pattern,value in self.get_values().items():
+            model.x[pattern] = value
+
         def bar_rule(model,length):
-            if self.bars[length] is not None:
-                patrns = filter(lambda p: p.length==length, self.patterns)
-                return sum(model.x[p] for p in patrns) <= self.bars[length]
+            if self.bars[length] != -1:
+                ptrns = list(filter(lambda p: p.length==length, self.patterns))     
+                if len(ptrns) == 0:
+                    return pyo.Constraint.Skip
+                return sum(model.x[p] for p in ptrns) <= self.bars[length]
             else:
-                pyo.Constraint.Skip()
-        self.model.pieces_rule = pyo.Constraint(self.bars.keys(), rule=bar_rule)
+                return pyo.Constraint.Skip
+        model.bar_rule = pyo.Constraint(self.bars.keys(), rule=bar_rule)
 
         def pieces_rule(model,length):
-            return sum(model.x[p] * p.get(length,0) for p in self.patterns) >= self.pieces[length]
+            return sum(model.x[p] * p.pieces.get(length,0) for p in self.patterns) >= self.pieces[length]
         model.pieces_rule = pyo.Constraint(self.pieces.keys(), rule=pieces_rule)
-        model.obj = pyo.Objective(expr=sum(model.x[p] * self.patterns[p].length for p in self.patterns))
+        model.obj = pyo.Objective(expr=sum(model.x[p] * p.length for p in self.patterns))
 
-        if domain == pyo.NonNegativeIntegers and (self.pattern_num is not None):
+        if domain == pyo.NonNegativeIntegers and (self.pattern_num != -1):
             model.y = pyo.Var(self.patterns, domain=pyo.Binary)
             def used_rule(model,pattern):
                 return model.x[pattern] <= model.y[pattern] * max([self.pieces[l]/pattern.pieces[l] for l in pattern.pieces.keys()])
@@ -70,7 +92,7 @@ class Parent():
             model.max_rule = pyo.Constraint(rule=max_rule)
 
         model.dual = pyo.Suffix(direction= pyo.Suffix.IMPORT)
-        self.solver.solve(self.model)
+        self.solver.solve(model)
         results = {
             'values': [(p,pyo.value(model.x[p])) for p in self.patterns],
             'obj': pyo.value(model.obj),
@@ -79,23 +101,24 @@ class Parent():
             results['duals'] = {length:model.dual[model.pieces_rule[length]] for length in model.pieces_rule}
         self.results = results
     
-    def get_duals(self):
+    def get_duals(self) -> Dict[int,float]:
         return self.results['duals']
     
-    def get_results(self):
+    def get_values(self):
         return self.results['values']
 
     def add_pattern(self, pattern: Pattern):
         self.patterns.append(pattern)
 
+
 class Child():
-    def __init__(self, solver: pyo.SolverFactory, lengths: List[int], blade_width: int, blade_num: Union[int,None]) -> None:
+    def __init__(self, solver: pyo.SolverFactory, lengths: List[int], blade_width: int, blade_num: int) -> None:
         self.solver = solver
         self.lengths = lengths
         self.blade_width = blade_width
         self.blade_num = blade_num
     
-    def solve(self, duals:List, bar_length: int):
+    def solve(self, duals:Dict[int,float], bar_length: int):
         model = pyo.ConcreteModel()
         model.z = pyo.Var(self.lengths, within=pyo.NonNegativeIntegers)
         model.s = pyo.Var(within=pyo.NonNegativeReals)
@@ -111,7 +134,7 @@ class Child():
 
         model.slack_lower_rule = pyo.Constraint(rule=slack_lower_rule)
         model.slack_upper_rule = pyo.Constraint(rule=slack_upper_rule)
-        if self.blade_num is not None:
+        if self.blade_num != -1:
             def blade_num_rule(model):
                 return sum(model.z[l] for l in self.lengths) <= self.blade_num + 1 - model.w
             model.blade_num_rule = pyo.Constraint(rule=blade_num_rule)
--- opticut/script.py
+++ opticut/script.py
@@ -1,11 +1,13 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple
 import pyomo.environ as pyo
+import click
+import json
+from optimization import CG, Pattern
+
 
 class SolverError(ValueError):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-
-    
 
 class CuttingStock():
     def __init__(self, solver_name:str) -> None:
@@ -14,33 +16,62 @@ class CuttingStock():
         else:
             self.solver = pyo.SolverFactory(solver_name)
 
-
-    def solve(
+    def solve(self,
             pieces: Dict[int,int],
-            bars: Dict[int, Union[None,int]] = None,
-            std_bar: Union[None,int] = None,
+            bars: Dict[int, int],
             blade_width: int = 0,
-            pattern_num: Union[None,int] = None,
-            blade_num: Union[None,int] = None
-        ) -> Dict: 
+            pattern_num: int = -1,
+            blade_num: int = -1
+        ) -> List[Tuple[Pattern,int]]:
         """_summary_
 
-        :param pieces: dict of pieces' length and their corresponding number.
+        :param pieces: Dict of pieces' length and their corresponding number. Lengths should be integer.
         :type pieces: Dict[int,int]
-        :param bars: dict of the available bars' length and their corresponding number, If the specified number is None there is no limit. defaults to None
-        :type bars: Dict[int, Union[None,int]], optional
-        :param std_bar: tandard length of the bar. Just one of the bars and std_bar should be given, defaults to None
-        :type std_bar: int, optional
-        :param blade_width:  the width of the cutting blade, defaults to 0
+        :param bars: Dict of the available bars' length and their corresponding number. Lengths should be integer.
+        :type bars: Dict[int, int]
+        :param blade_width: The width of the cutting blade, defaults to 0.
         :type blade_width: int, optional
-        :param pattern_num: maximum number of the used patterns and if is None there is no limit, defaults to None
+        :param pattern_num: maximum number of the used patterns and if is -1 there is no limit, defaults to -1.
         :type pattern_num: int, optional
-        :param blade_num:  maximum number of the cutting blades and if is None there is no limit, defaults to None
+        :param blade_num: maximum number of the cutting blades and if is -1 there is no limit, defaults to -1.
         :type blade_num: int, optional
         :return: _description_
         :rtype: Dict
-        """    
-        
+        """
+        cg = CG(self.solver, pieces, bars, blade_width, pattern_num, blade_num)
+        cg.solve()
 
-        pass
 
+
+@click.command()
+@click.argument('input', type=click.File('r'))
+@click.argument('output', type=click.File('w'))
+@click.option('--blade-width', help="blade's width")
+@click.option('--blade-num', help='maximum number of blades')
+@click.option('--pattern-num', help='maximum number of used patterns')
+@click.option('--solver-name', default='glpk', help='name of the solver in lowercase')
+def cli(input, output, blade_width, blade_num, pattern_num, solver_name):
+    """Copy contents of INPUT to OUTPUT."""
+    cli_options = {
+        'blade_width' : blade_width,
+        'blade_num' : blade_num,
+        'pattern_num' : pattern_num,
+    }
+    input_data = json.load(input)
+    data = {}
+    for item in ('pieces', 'bars'): 
+        data[item] = {p['length']:p['quantity'] for p in input_data[item]}
+
+    input_options = input_data.get('options', {})
+
+    options = {}
+    for option in cli_options:
+        if cli_options[option] is not None:
+            options[option] = cli_options[option]
+        elif option in input_options:
+            options[option] = input_options[option]
+    cs = CuttingStock(solver_name)
+    cs.solve(**data, **options)
+
+if __name__ == '__main__':
+    cli()
